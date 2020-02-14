8ff33162d29d2870d8d6ba7c000a141ed5616276
Thomas Reynolds <48138681+thomasreynolds4881@users.noreply.github.com>
2020-02-14 13:36:25 -0600

    Update VexRobotVideoCode.ino
    Changed name of file to vexMotorRobotControl, now works with just one motor, updated motorControl function to take a Servo argument.

--- VexRobotVideoCode.ino
+++ VexRobotVideoCode.ino
@@ -1,37 +1,52 @@
+//************************** vexMotorRobotControl *****************************************
+// Created By: Thomas Reynolds, Luke Gentle, using code from Evan Weinberg
+// 
+// Created On: 2/14/2020
+//
+// Last Modified: 2/14/2020
+//
+// Purpose: 
+//
+//*****************************************************************************************
+
 
 #include <Servo.h>
 
-int leftMotorPin = 4; //left motor pin
-int rightMotorPin = 2; //right motor pin
-Servo leftMotor;
-Servo rightMotor; 
 
+const int leftMotorPin = 5;       //Left motor pin
+const int LEDPin = 13;            //LED pin
 
+Servo leftMotor;                  //Define variables for each motor
 
-void setup() {
+void setup() 
+{
   // put your setup code here, to run once:
-  pinMode(leftMotorPin,OUTPUT);
-  pinMode(rightMotorPin, OUTPUT);
-  pinMode(13,OUTPUT); 
-  leftMotor.attach(leftMotorPin); 
-  rightMotor.attach(rightMotorPin); 
-}
+  pinMode(leftMotorPin,OUTPUT);   //Set leftMotorPin to be an output pin.
+  pinMode(LEDPin,OUTPUT);         //Set LEDPin to be an output pin.
+  
+  leftMotor.attach(leftMotorPin); //leftMotor will be controlled by leftMotorPin
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  leftMotorControl(100); 
-  rightMotorControl(100);
-  digitalWrite(13,HIGH);
+}//End of setup
+
+// put your main code here, to run repeatedly:
+void loop() 
+{
+  //Send full forward to both motors
+  motorControl(leftMotor, 100);
+  digitalWrite(LEDPin,HIGH);      //Turn the LEDpin on
+  delay(1000);                    //Wait 1000 ms, or one second
+
+  
+  motorControl(leftMotor, -100);  //Send full backward to both motors
+  digitalWrite(LEDPin,LOW);       //Turn the LEDpin off
   delay(1000);
-  leftMotorControl(-100);
-  rightMotorControl(-100);
-  digitalWrite(13,LOW):
-  delay(1000);
-}
+  
+}//End of loop
 
-int leftMotorControl(int value){
- 
-leftMotor.write(map(value,-100,100,1000,2000));
 
-}
-
+//The functions below let you send a power value between -100 and 100 to the motors. 
+//The servo library uses numbers between 1000 and 2000 to control servos. 
+int motorControl(Servo motor, int value)
+{
+  motor.write(map(value,-100,100,1000,2000));
+}//End of motorControl
